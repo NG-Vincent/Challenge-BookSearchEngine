cb7e9c90d6a048a5fd9eacb228a502e1859c5f1d
Kelerik <vincent.ng39@gmail.com>
2022-03-15 18:58:41 -0400

    update typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,25 +6,35 @@ const typeDefs = gql`
       _id: ID
       username: String
       email: String
-      password: String
+      bookCount: Int
       savedBooks: [Book]
    }
+
    type Book {
-      authors: String
-      description: String
       bookId: String
+      authors: [String]
+      description: String
+      title: String
       image: String
       link: String
-      title: String
    }
 
    type Query {
-      user: User
-      books: [Book]
+      me: User
    }
 
    type Mutation {
       login(email: String!, password: String!): Auth
+      addUser(username: String!, email: String!, password: String!): Auth
+      saveBook(
+         authors: [String]!
+         description: String!
+         title: String!
+         bookId: String!
+         image: String!
+         link: String
+      ): User
+      removeBook(bookId: String!): User
    }
 
    type Auth {
