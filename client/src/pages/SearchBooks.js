c056adf6704c7db17808160dbcd2092765a7becd
Kelerik <vincent.ng39@gmail.com>
2022-03-16 18:29:55 -0400

    update comments

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -71,26 +71,6 @@ const SearchBooks = () => {
       // find the book in `searchedBooks` state by the matching id
       const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-      // // get token
-      // const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-      // if (!token) {
-      //    return false;
-      // }
-
-      // try {
-      //    const response = await saveBook(bookToSave, token);
-
-      //    if (!response.ok) {
-      //       throw new Error("something went wrong!");
-      //    }
-
-      //    // if book successfully saves to user's account, save book id to state
-      //    setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-      // } catch (err) {
-      //    console.error(err);
-      // }
-
       try {
          // save book
          await saveBook({
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,31 +19,36 @@ const resolvers = {
    Mutation: {
       addUser: async (parent, args) => {
          const user = await User.create(args);
+         // return a JWT to stay logged in
          const token = signToken(user);
-
          return { token, user };
       },
       login: async (parent, { email, password }) => {
+         // check if user exists
          const user = await User.findOne({ email });
-
          if (!user) {
             throw new AuthenticationError("Incorrect credentials");
          }
 
+         // check if password is correct
          const correctPw = await user.isCorrectPassword(password);
-
          if (!correctPw) {
             throw new AuthenticationError("Incorrect credentials");
          }
 
+         // return a JWT to stay logged in
          const token = signToken(user);
          return { token, user };
       },
       saveBook: async (parent, args, context) => {
+         // if logged in
          if (context.user) {
+            // find user by id
             const bookData = await User.findByIdAndUpdate(
                { _id: context.user._id },
+               // add new book data to user
                { $push: { savedBooks: args } },
+               // return newly updated data
                { new: true }
             );
 
